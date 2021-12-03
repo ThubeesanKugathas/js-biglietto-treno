ba759b73a6be51a8cd7670921df23f0329aaff70
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-03 14:44:47 +0100

    js with console.log

--- js/script.js
+++ js/script.js
@@ -2,15 +2,39 @@
 // richiesta dei km che si vogliono percorrere
 const distance = parseInt(prompt('Quanti KM vuole percorrere?'));
 // richiesta age dell'utente
-const age = parseInt(prompt('In che anno è nato?'));
+const birthYear = parseInt(prompt('In che anno è nato?'));
 
 // calcolo prezzo puro senza sconti
-// let firstprice = distance * 0.21;
+let price = distance * 0.21;
 
-// sconto basato dalla età
-// let discountedvalue = (1 - discount );
+console.log(price);
 
+// calcolo età
+const now = new Date();
 
+console.log(now);
+
+let age = now.getFullYear() - birthYear;
+
+console.log(age);
+
+// calcolo discount secondo l'età
+if (age < 18) {
+    discount = 0.20;
+} else if (age < 65) {
+    discount = 0;
+} else {
+    discount = 0.40;
+}
+
+let discountedValue = (1 - discount );
+
+console.log(discountedValue);
 
 // prezzo finale dato dalla moltiplicazione del firstprice e il valore di sconto
-// let price = firstprice * discountedvalue;
+let totPrice = fixedPrice * discountedValue;
+
+// decimali per i centesimi
+let fixedTotPrice = totPrice.toFixed(2);
+
+console.log(fixedTotPrice);
