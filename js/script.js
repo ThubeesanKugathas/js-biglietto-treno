dfa4b79b6fcb58eb6403e00452b2840a643fd0e8
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-03 13:59:42 +0100

    comments js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// creazione prezzo del biglietto treno 
+// richiesta dei km che si vogliono percorrere
+const distance = parseInt(prompt('Quanti KM vuole percorrere?'));
+// richiesta age dell'utente
+const age = parseInt(prompt('In che anno è nato?'));
+
+// calcolo prezzo puro senza sconti
+// let firstprice = distance * 0.21;
+
+// sconto basato dalla età
+// let discountedvalue = (1 - discount );
+
+
+
+// prezzo finale dato dalla moltiplicazione del firstprice e il valore di sconto
+// let price = firstprice * discountedvalue;
